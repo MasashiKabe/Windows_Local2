24e6f6fc5e3d2396360414a3f9acd50d793e4785
MasashiKabe <mgmn3019@gmail.com>
2021-11-10 01:45:05 +0900

    PHP_Laravel17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -19,17 +20,6 @@ class ProfileController extends Controller
         $this->validate($request, Profile::$rules);
         $profile = new Profile;
         $form = $request->all();
-
-        //formに画像があれば保存する
-        if ($form['image']) {
-            $path = $request->file('image')->store('public/image');
-            $profile->image_path = basename($path);
-        } else {
-            $profile->image_path = null;
-        }
-        
-        unset($form['_token']);
-        unset($form['image']);
         //データベースに保存する
         $profile->fill($form);
         $profile->save();
@@ -68,17 +58,6 @@ class ProfileController extends Controller
         $profile = Profile::find($request->id);
         //送信されてきたフォームデータを格納する
         $profile_form = $request->all();
-        if ($request->remove == 'true') {
-            $profile_form['image_path'] = null;
-        } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $profile_form['image_path'] = basename($path);
-        } else {
-            $profile_form['image_path'] = $profile->image_path;
-        }
-
-        unset($profile_form['image']);
-        unset($profile_form['remove']);
         unset($profile_form['_token']);
 
         //該当するデータを上書きして保存する
@@ -86,4 +65,15 @@ class ProfileController extends Controller
 
         return redirect('admin/profile');
     }
+
+    public function delete(Request $request)
+    {
+        //該当するProfile Modelを取得
+        $profile = Profile::find($request->id);
+        //削除する
+        $profile->delete();
+        return redirect('admin/profile/');
+    }
+
+
 }

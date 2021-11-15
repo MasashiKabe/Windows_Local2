99f6309e720846b8f5b838bb9d86f73123dc68e5
MasashiKabe <mgmn3019@gmail.com>
2021-11-15 22:21:18 +0900

    heroku課題

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.admin')
-@@section('title', '登録済みニュースの一覧')
+@section('title', '登録済みニュースの一覧')
 
 @section('content')
     <div class="container">
         <div class="row">
             <h2>ニュース一覧</h2>
+            <p>heroku課題</p>
         </div>
         <div class="row">
             <div class="col-md-4">

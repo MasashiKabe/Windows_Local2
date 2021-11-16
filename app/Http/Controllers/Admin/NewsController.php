160d18ffc1fc55e4cfaf26dac00d1d9e7fb8c272
MasashiKabe <mgmn3019@gmail.com>
2021-11-16 23:56:30 +0900

    へろく画像

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -87,7 +87,7 @@ class NewsController extends Controller
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
             $path = Storage::disk('s3')->putfile('/',$form['image'],'public');
-            $news_form['image_path'] = Storage::disk('s3')->url($path); 
+            $news->image_path = Storage::disk('s3')->url($path); 
         } else {
             $news_form['image_path'] = $news->image_path;
         }
